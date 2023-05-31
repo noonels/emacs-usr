f7fdce0c8044be1b51f8126354d66cf6a3e5ff12
M.Cooper Healy <m.cooper.healy@gmail.com>
2023-05-31 11:24:24 -0500

    Add file for utility funcions

---
+++ utils.el
@@ -0,0 +1,9 @@
+;;; utils.el --- Utility functions -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;; Author: M Cooper Healy <m.cooper.healy@gmail.com>
+;;
+;;; Code:
+
+(provide 'utils)
+;;; utils.el ends here
